b3bc389b807d7881d17dda8fd6083ef3804075f2
Kyle Bowen <kbowe002@hammer.cs.ucr.edu>
2017-10-31 21:34:37 -0700

    ~

--- src/Parse.h
+++ src/Parse.h
@@ -1,13 +1,15 @@
 #ifndef PARSE_H
 #define PARSE_H
-
+#include <sstream>
 #include <string>
 
 class Parse() {
     private:
         vector<char*> argList;
     public:
-        Parse(vector<string> &vStr);
+        Parse(vector<string> &vStr) {
+		
+	}
 };
 
 
