5c03cf4fe9fbf58ea3b9be1f69fbb59fa87e703d
Andrew Munoz <amuno032@ucr.edu>
2017-10-31 21:28:18 -0700

    Added prototypes to parse.h

--- src/Parse.h
+++ src/Parse.h
@@ -1,6 +1,15 @@
 #ifndef PARSE_H
 #define PARSE_H
+
+#include <string>
+
 class Parse() {
-	
+    private:
+        vector<char*> argList;
+    public:
+        Parse(vector<string> &vStr);
 };
+
+
+
 #endif
