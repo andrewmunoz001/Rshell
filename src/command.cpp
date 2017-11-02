a12f42b52602e5784c9105f7ae9c073369e08393
Andrew Munoz <amuno032@ucr.edu>
2017-11-01 21:42:08 -0700

    Added execute functions for command class. Should be able to handle connector algorithms

--- src/command.cpp
+++ src/command.cpp
@@ -14,6 +14,71 @@ vector<string> Command::getCmdStr(){
     return rStr;
 }
 
+bool Command::executeCommand(){
+    unsigned elements = cmdStr.size();
+    int returnStatus;
+
+    // Exit function
+    if (cmdStr.at(0) == "exit"){
+        if (cmdStr.size() != 1){
+            printf("rshell: Error, too many arguments for exit!\n");
+            return false;
+        }
+        else
+            exit(0);      // terminates the program
+    }
+
+    char** args = new char*[elements + 1];  // needs 1 extra for the NULL char*
+
+    for (unsigned i = 0; i < elements; i++){ // fill up char array with the arguments
+        args[i] = (char*)(cmdStr.at(i).c_str());
+    }
+    args[elements] = NULL; // make sure last element is set to null (For execvp to work)
+
+    pid_t pid = fork(); // fork value set to pid, of type pid_t
+
+    if (pid < 0){           // if pid < 0, error in forking
+        perror("ForkFailure");
+    }
+    if (pid == 0){       // if child process
+        if (execvp(args[0], args) == -1){   // execvp(char* cmd, char* arg[])
+            perror("exec");
+            exit(111);    
+        }
+    }
+    if (pid > 0){       // parent process
+        int statval;
+        waitpid(-1, &statval, 0);     // wait for child to be done
+        returnStatus = WEXITSTATUS(statval);
+        if (returnStatus == 111)
+            return false;          // statement did NOT execute
+        else
+            return true;            // no error in execvp, statement executed 
+    }
+
+    return false;
+}
+
+bool Command::executeNext(){
+    if (connectorType  == "||"){     // logic for "||"
+        if (executeCommand())  
+            return false;     // statement executed, no need to execute the next command
+        return true;          // otherwise, it failed, so execute the next command
+    }
+    if (connectorType  == "&&"){     // logic for "&&"
+        if (executeCommand())  
+            return true;     // statement executed, execute the next command
+        return false;          // otherwise, it failed, dont execute next command
+    }
+    if (connectorType  == ";"){     // logic for ";"
+        executeCommand();  
+        return true;          // next command executes no matter what 
+    }
+    else
+        return false;    // if it doesnt have a connector, it has no next command to execute
+
+}
+
 void Command::showCommand(){
     cout << endl << "arg list: ";
     for (unsigned i = 0; i < cmdStr.size(); i++){
--- src/command.h
+++ src/command.h
@@ -3,6 +3,11 @@
 
 #include <string>
 #include <vector>
+#include <unistd.h> // fork() and execvp()
+#include <sys/types.h>      // waitpid()
+#include <sys/wait.h>       // waitpid()
+#include <stdio.h>          // perror
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,6 +18,8 @@ class Command {
     public:
         // Connector will be initilized to empty string, unless a connector is passed through
         Command(const vector<string>& iVector, const string& con);
+        bool executeCommand();      // executes one command
+        bool executeNext();         // uses connectors 
         vector<string> getCmdStr();     // returns the vector of commands
         void showCommand();     // shows whats in the command object, used for testing
 };
