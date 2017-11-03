7cecb4aca0df86a9991038a20bc440e4de0009ff
Andrew Munoz <amuno032@ucr.edu>
2017-11-03 11:10:42 -0700

    Cleaned up shell main

--- src/shell.cpp
+++ src/shell.cpp
@@ -11,7 +11,6 @@
 using namespace boost;
 using namespace std;
 
-void execute(Command& );
 
 int main(int argc, char* argv[]){
 
@@ -42,45 +41,4 @@ int main(int argc, char* argv[]){
 		}
 	}
 	return 0;
-}
-
-
-void execute(Command& inCmd){
-	vector<string> execVector = inCmd.getCmdStr();
-	unsigned elements = execVector.size();
-
-	// Exit function
-	if (execVector.at(0) == "exit"){
-		if (execVector.size() != 1){
-			printf("rshell: Error, too many arguments for exit!\n");
-			return;
-		}
-		else {
-			exit(0);       // terminate normally    
-		}
-	}
-
-	char** args = new char*[elements + 1];       // needs 1 extra for the NULL char*
-
-	for (unsigned i = 0; i < elements; i++){     // fill up char array with the arguments
-		args[i] = (char*)(execVector.at(i).c_str());
-	}
-	args[elements] = NULL;         // make sure last element is set to null
-
-
-	pid_t pid = fork();                    // fork value set to pid, of type pid_t
-
-	if (pid < 0){   // if pid < 0, error in forking
-		perror("ForkFailure");
-	}
-	if (pid == 0){  // if child process
-		if (execvp(args[0], args) == -1){     // execvp(char* cmd, char* arg[])
-			perror ("exec");
-		}
-		exit(1);
-	}
-	if (pid > 0){    // parent process
-		wait(NULL);
-	}
-	delete [] args;       // make sure to deallocate the memory
 }
