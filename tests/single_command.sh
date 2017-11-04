d3fe9a37a7a0c3efcceeb205e0d74dc18785a51d
Kyle Bowen <kbowe002@hammer.cs.ucr.edu>
2017-11-03 20:12:42 -0700

    single_command seems good

--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo 'ls; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
-echo 'ls -a; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
-echo 'echo Hello, this is rshell; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
-echo 'ps; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
+echo 'ls; exit' | ./bin/rshell
+echo
+echo 'ls -a; exit' | ./bin/rshell
+echo
+echo 'echo Hello, this is rshell; exit' | ./bin/rshell
+echo
+echo 'ps; exit' | ./bin/rshell
+echo
