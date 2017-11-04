b23a3e1109df1d8e3655456e7d77a34993e610b7
Kyle Bowen <kbowe002@hammer.cs.ucr.edu>
2017-11-03 19:52:42 -0700

    fixed paths in single_command.sh

--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'ls; exit' | .././bin/rshell
-echo 'ls -a; exit' | .././bin/rshell
-echo 'echo Hello, this is rshell; exit' | .././bin/rshell
-echo 'ps; exit' | .././bin/rshell
+echo 'ls; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
+echo 'ls -a; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
+echo 'echo Hello, this is rshell; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
+echo 'ps; exit' | /class/classes/kbowe002/cs100Assignment/rshell-pico-de-gallo/bin/rshell
