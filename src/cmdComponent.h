5e741684e6bf36dd061c4b2f42b8fa8b83bf3e37
Andrew Munoz <amuno032@ucr.edu>
2017-11-11 15:24:15 -0800

    Added virtual destructor for component

--- src/cmdComponent.h
+++ src/cmdComponent.h
@@ -1,9 +1,13 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include <iostream>
+using namespace std;
 class cmdBase{
     public:
         cmdBase(){}
+        virtual ~cmdBase(){};
+            
         virtual bool executeCommand() = 0;
 };
 
