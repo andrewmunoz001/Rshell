7d33735e984fd63f26c3546de879a5b2c15d77d3
Andrew Munoz <amuno032@ucr.edu>
2017-11-12 18:26:15 -0800

    Updated exit command script to test exit with precedence and test commands

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo 'echo this will print then exit; exit' | ../bin/rshell
-echo 'echo this prints 1st && echo this prints 2nd, then exits && exit' | ../bin/rshell
-
+echo 'echo TEST 1: && exit' | ../bin/rshell
+echo "echo TEST 2: && test ./idontexist || exit" | ../bin/rshell
+echo "echo TEST 3: || echo test failed && echo testing... && exit" | ../bin/rshell
+echo "echo TEST 4: || idontexist && idontexist; exit" | ../bin/rshell
+echo "echo TEST 5: && (echo testing || echo FAILED) && exit" | ../bin/rshell
+echo "echo TEST 6: && (((echo testing with nested parenthesis. || echo FAILED) || echo FAILED) && echo testing..); exit" | ../bin/rshell
+echo "echo TEST 7: && (echo testing with nests and test cmd && (test ./idontexist || exit))" | ../bin/rshell
+echo "echo TEST 8: && echo testing with test cmd && test -d ../bin && exit" | ../bin/rshell
+echo "echo TEST 9: && echo testing with test cmd && test -f ../bin/rshell && exit" | ../bin/rshell
+echo "echo TEST 10: && echo testing with failed cmd test && [ -d ../bin/rshell ] || exit" | ../bin/rshell
