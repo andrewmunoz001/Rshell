0507004e91ad31f889f57cbc1ef3af125218004b
Andrew Munoz <amuno032@ucr.edu>
2017-12-08 01:05:35 -0800

    Added tests for piping

--- tests/pipe_test.sh
+++ tests/pipe_test.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
 ../bin/rshell <<EOF
+echo "*****     Normal Piping Tests     *****"
+echo "Test 1: ls | grep pipe"
+ls | grep pipe 
+echo "Test 2: ps | grep rshell"
+ps | grep rshell
+echo "Test 3: ls -a | grep command | sort"
+ls -a | grep command | sort
+
+echo "****** Tests with an invalid pipe command"
+echo "Test 1: ls | idontexist || echo TEST SUCCESS"
+ls | idontexist || echo TEST SUCCESS
+echo "Test 2:  idontexist | grep | sort || echo TEST SUCCESS"
+idontexist | grep pipe | sort || echo TEST SUCESS
+echo "Test 3: cat pipe_test.sh | sort | idontexist || TEST SUCCESS"
+cat pipe_test.sh | sort | idontexist || echo TEST SUCCESS
+
+echo "***** Tests with multiple pipes   *****"
+echo "Test 1: ls | grep pipe | tr a-z A-Z"
+ls | grep pipe | tr a-z A-Z
+echo "Test 2: ls | grep pipe | wc"
+ls | grep pipe | wc
+
+echo "***** Tests that implement everything   ******"
+echo "Test 1: cat < pipe_test.sh | grep pipe | sort | tr a-z A-Z > test1output"
+cat < pipe_test.sh | grep pipe | sort | tr a-z A-Z > test1output
+echo "Test 2: ls | tr a-z A-Z | wc > test2output"
+ls | tr a-z A-Z | wc > test2output
 
 exit
 EOF
